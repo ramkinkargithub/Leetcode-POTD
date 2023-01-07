2e6176b84c8bcc70e8074d25e4f520bfb58d8e63
Ram Kinkar Singh <ramkinkar2512@gmail.com>
2023-01-08 00:01:58 +0530

    Runtime100 ms Beats 66.30% Memory69.3 MB Beats 90.73%

--- January 2023/134. Gas Station.cpp
+++ January 2023/134. Gas Station.cpp
@@ -76,3 +76,26 @@ public:
       return -1;
     }
 };
+
+//O(n)
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+      int totalGas=0,totalCost=0,ansIndex=0,currGas=0;
+      for(int i=0;i<gas.size();i++){
+        totalGas+=gas[i];
+        totalCost+=cost[i];
+        currGas+=gas[i];
+
+        if(currGas<cost[i]){
+          ansIndex=i+1;
+          currGas=0;
+        }
+        else
+          currGas-=cost[i];
+      }
+      if(totalGas<totalCost)
+        return -1;
+      return ansIndex;
+    }
+};
